c7943e036639c055274f390ddcf81cc946153d4a
PeterDelwita <peterd72@nycstudents.net>
2024-10-27 12:51:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -25,3 +25,20 @@ function binaryToDecimal() {
 }
 
 binaryToDecimal();
+
+function decimalToBinary(decimal) {
+  let binary = "";
+  let power = 7;
+  while (decimal > 0) {
+    if (2 ** power < decimal) {
+      decimal -= 2 ** power;
+      binary += "1"; // Needs fixing
+    } else if (2 ** power > decimal) {
+      binary += "0";
+    }
+    power -= 1;
+  }
+  console.log(binary);
+}
+
+decimalToBinary(255);
