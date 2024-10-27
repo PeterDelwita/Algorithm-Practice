33c32e287954bfcc48a49e188c147314c9768099
PeterDelwita <peterd72@nycstudents.net>
2024-10-27 12:28:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-function getFactorial(n) {
-  if (n < 0) {
-    // Selection
-    return;
+// function getFactorial(n) {
+//   if (n < 0) {
+//     // Selection
+//     return;
+//   }
+//   let factorial = 1; // Sequencing (see above and below)
+//   for (let i = 1; i <= n; i++) {
+//     // Iteration
+//     factorial *= i;
+//   }
+//   console.log(factorial);
+// }
+
+// getFactorial(7);
+
+function binaryToDecimal() {
+  const binary = "01101110";
+  let decimal = 0;
+  for (let i = 0; i <= 7; i++) {
+    if (binary[i] == "1") {
+      decimal += 2 ** (7 - i);
+    }
   }
-  let factorial = 1; // Sequencing (see above and below)
-  for (let i = 1; i <= n; i++) {
-    // Iteration
-    factorial *= i;
-  }
-  console.log(factorial);
+  console.log(decimal);
 }
 
-getFactorial(7);
+binaryToDecimal();
